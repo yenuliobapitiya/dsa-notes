2978de5343d3e815a9e29d551cee831667a65904
Yenuli Obapitiya <yenuliobapitiya@gmail.com>
2025-06-15 01:55:03 +0530

    bubble sort example

--- sorting_algorithm/BubbleSort.java
+++ sorting_algorithm/BubbleSort.java
@@ -13,13 +13,18 @@ public class BubbleSort {
         }
 
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size-1; j++) {
-                if (nums[j] > nums[j + 1]) {
+            for (int j = 0; j < size-i-1; j++) {
+                if (nums[j] > nums[j + 1]) 
+                {
                     temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
                 }
 
+            }
+            System.out.println();
+            for(int num : nums){
+                System.out.println(num + " ");
             }
         }
 
