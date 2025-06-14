b3db08a7f206ac4394f5de90efba7a22b7cffbb1
Yenuli Obapitiya <yenuliobapitiya@gmail.com>
2025-06-15 01:33:19 +0530

    example for bubble sort

--- sorting_algorithm/BubbleSort.java
+++ sorting_algorithm/BubbleSort.java
@@ -1,5 +1,20 @@
 package sorting_algorithm;
 
 public class BubbleSort {
-    
+    public static void main(String[] args) {
+
+        int nums[] = { 6, 5, 2, 8, 9, 4 };
+
+        System.out.println("before sorting");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+
+        System.out.println("after sorting");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
 }
